0f2ca7b0b9f627d010128194d4a30166bfe5a514
github9coder <optimusprinceps.traianus.dacicus@gmail.com>
2024-10-06 11:56:10 +0200

    Update Eros.py

--- HelioSystem/pages/Eros.py
+++ HelioSystem/pages/Eros.py
@@ -56,7 +56,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-st.image("/workspaces/NasaSpaceApps2024/HelioSystem/imagesEros.webp", width = 750)
+st.image("HelioSystem/images/Eros.webp", width = 750)
 
 
 with st.chat_message("assistant"):
