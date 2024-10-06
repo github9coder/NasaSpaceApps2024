5007579c344c95d19b1400d95b3fd4d6e52a6b11
github9coder <optimusprinceps.traianus.dacicus@gmail.com>
2024-10-06 11:56:50 +0200

    Update Hygiea.py

--- HelioSystem/pages/Hygiea.py
+++ HelioSystem/pages/Hygiea.py
@@ -56,7 +56,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-st.image("/workspaces/NasaSpaceApps2024/HelioSystem/imagesHygiea.webp", width=750)
+st.image("HelioSystem/images/Hygiea.webp", width=750)
 
 
 with st.chat_message("assistant"):
