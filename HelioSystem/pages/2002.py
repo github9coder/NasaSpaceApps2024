2e38d796f26a61c7207ee435db59477f00f35282
github9coder <optimusprinceps.traianus.dacicus@gmail.com>
2024-10-06 11:43:40 +0200

    Update 2002.py

--- HelioSystem/pages/2002.py
+++ HelioSystem/pages/2002.py
@@ -56,7 +56,7 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-st.image("/workspaces/NasaSpaceApps2024/HelioSystem/images/HalleysComet.jpeg", width = 750)
+st.image("HelioSystem/images/HalleysComet.jpeg", width = 750)
 
 
 with st.chat_message("assistant"):
